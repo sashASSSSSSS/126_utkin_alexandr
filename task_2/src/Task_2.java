68d00f8ac30459e7c55e8e23da1cdbdc25e9a632
sashASSSSSSS <90263225+sashASSSSSSS@users.noreply.github.com>
2021-10-25 15:59:23 +0300

    g

---
+++ task_2/src/Task_2.java
@@ -0,0 +1,61 @@
+public class Task_2 implements Task_2_base {
+    @Override
+    public int subtask_1_while(int num) {
+        double a = Math.sqrt(num);
+        double b =Math.floor(a);
+        double h= b*b;
+        return (int) h;
+
+
+        // Найти максимальное число, являющееся полным квадратом,
+        // не превосходящее заданное натуральное num
+
+
+    }
+    @Override
+    public int subtask_2_while(int num) {
+        if(num==0){
+            return 1;
+        }
+        else{
+            return 2* subtask_2_while(num-1) +6;
+        }
+        // Последовательность целых чисел p(n) определяется следующим образом:
+        // p(0) = 1
+        // p(k) = 2 * p(k - 1) + 6, k > 0
+        //Найти элемент последовательности с номером num
+
+    }
+
+    @Override
+    public boolean subtask_3_while(int num, int base) {
+        for(int i = 0; i < num; i++ ){
+            if(Math.pow(base, i) == num ){
+                return true;
+            }
+        }
+        // Проверить, является ли число num натуральной степенью числа base
+        return false;
+    }
+
+    @Override
+    public int subtask_4_while(int start, int end) {
+        if (start==end){
+            return 0;
+        }
+        if (end>start){
+            return end-start;
+        }
+        if (start%2==1){
+            return 1 + subtask_4_while(end, start-1);
+        }
+        else {
+            return 1 + subtask_4_while(end, start/2);
+        }
+        // Вычислить, за какое минимальное число операций
+        // вычесть 1
+        // поделить на 2
+        // число start можно превратить в end. Гарантируется, что start >= end >= 1
+
+    }
+}
