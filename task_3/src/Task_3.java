c31f1366be33aaae8e7918b4616b43eadb39980b
sashASSSSSSS <90263225+sashASSSSSSS@users.noreply.github.com>
2021-10-30 22:24:58 +0300

    tfgt

--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -2,13 +2,13 @@ public class Task_3 implements Task_3_base {
     @Override
     public int subtask_1_for(int n1, int n2, int a, int b) {
         int k =0;
-        for(int i = n1-1; i<=n2 ;i++){
-            if(i%a==0) {
-                if(i%b!=0){
-                    k++;
-                }
+        for(int i = n1; i<=n2 ;i++) {
+            if((i%a==0)&(i%b!=0)){
+                k++;
             }
         }
+
+
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
         return k;
@@ -16,13 +16,16 @@ public class Task_3 implements Task_3_base {
 
     @Override
     public int subtask_2_for(int num) {
-        int[] me = new int[1000000];
-        for(int i =1; i<=num; i++){
-            for(int j=1; j<=i; j++){
-                me[j]=i;
 
-            }
+        int n = 0;
+        int i = 0;
+        while(n<num){
+            i++;
+            n=n+i;
         }
+        return i;
+
+
         // Последовательность чисел строится следующим образом:
         // сначала идет одна единица,
         // потом две двойки,
@@ -32,24 +35,53 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        return me[num-1];
+
     }
 
     @Override
     public int subtask_3_for(int num, int d, int cnt) {
+        int k = 0;  //сумма
+        int n=num;  // n-ный член
+        if (cnt == 0) {
+            return num;
+        }
+        /*else {
+            return  subtask_3_for(num, d, (cnt-1)) * d + 1;
+        }
+        */
+        for(int i =1;i<=cnt-1; i++){
+            n=n*d+1;
+            k=k+n;
+        }
         // Дана последовательность
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
+        return k;
     }
 
     @Override
     public int subtask_4_for(int n) {
+        int res =1;
+        int s =0;
+        for(int i =1; i<=n; i++){
+            res = res * i;
+            s=s+res;
+        }
+
+        /*int s=0;
+        if(n<=1){
+            return 1;
+        }
+        else{
+            return s + n * subtask_4_for(n-1);
+        }
+
+         */
         // Вычислить сумму
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
+        return s;
     }
 }
