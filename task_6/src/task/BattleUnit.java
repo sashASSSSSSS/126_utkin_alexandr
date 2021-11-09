35f1b2ad904f515f616f59d984352288ff019912
sashASSSSSSS <90263225+sashASSSSSSS@users.noreply.github.com>
2021-11-09 22:57:10 +0300

    ds

---
+++ task_6/src/task/BattleUnit.java
@@ -0,0 +1,51 @@
+package task;
+
+    public class BattleUnit {
+        private int strength;
+        private int armor;
+        private int health;
+        private int x;
+        private int y;
+
+        public BattleUnit(int strength, int armor, int health, int x, int y) {
+            this.strength = strength;
+            this.armor = armor;
+            this.health = health;
+            this.x = x;
+            this.y = y;
+        }
+        public int getArmor() {
+            return armor;
+        }
+        public int getHealth() {
+            return health;
+        }
+        public int getStrength() {
+            return strength;
+        }
+        public int getX() {
+            return x;
+        }
+        public int getY() {
+            return y;
+        }
+        public void moveUp() {
+            this.y--;
+        }
+        public void moveDown() {
+            this.y++;
+        }
+        public void moveLeft() {
+            this.x--;
+        }
+        public void moveRight() {
+            this.x++;
+        }
+
+        public void attacked(BattleUnit enemy) {
+            this.health -= Math.min(enemy.getStrength(), Math.max(0, enemy.getStrength() - this.getArmor()));
+        }
+        public boolean isAlive() {
+            return this.health > 0;
+        }
+    }
